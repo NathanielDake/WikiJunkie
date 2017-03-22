c10b08025f240c2db34917c99bf3bd197dbef2ca
Nate <ndake11@gmail.com>
2017-03-22 08:32:55 -0400

    checkpoint 7: charges controller updated

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,50 +1,18 @@
 class ChargesController < ApplicationController
 include Amount
-
-def down_grade_to_standard
-  if current_user.update(role: "standard")
-    current_user.set_wikis_public
-    flash[:notice] = " You have downgraded your account to standard, #{current_user.email}!"
-    redirect_to root_path
-  else
-    flash[:alert] = "There appears to have been an error. Please try again."
-  end
-end
+include ChargesHelper
 
   def new
-    @stripe_btn_data = {
-        key: "#{ Rails.configuration.stripe[:publishable_key] }",
-        description: "BigMoney Membership - #{current_user.email}",
-        amount: default_amount
-    }
+    new_stripe_btn_data
   end
 
   def create
-    customer = Stripe::Customer.create(
-        email: current_user.email,
-        card: params[:stripeToken]
-    )
-
-    charge = Stripe::Charge.create(
-        customer: customer.id,
-        amount: default_amount,
-        description: "BigMoney Membership - #{current_user.email}",
-        currency: 'usd'
-    )
-
-    if charge.present? && current_user.update_attributes(role: 'premium')
-      flash[:notice] = "Thanks for the payment #{current_user.email}! You can now create and edit private wikis!."
-      redirect_to root_path
-    else
-      flash[:alert] = "There appears to have been an error. Please try again."
-    end
+    create_customer
+    create_charge
+    save_charge
   end
-
 
   rescue Stripe::CardError => e
     flash[:alert] = e.message
     redirect_to new_charge_path
-
-
-
 end
--- app/helpers/charges_helper.rb
+++ app/helpers/charges_helper.rb
@@ -1,2 +1,49 @@
 module ChargesHelper
+
+  def new_stripe_btn_data
+    @stripe_btn_data = {
+        key: "#{ Rails.configuration.stripe[:publishable_key] }",
+        description: "BigMoney Membership - #{current_user.email}",
+        amount: default_amount
+    }
+  end
+
+  def create_customer
+    @customer = Stripe::Customer.create(
+        email: current_user.email,
+        card: params[:stripeToken]
+    )
+  end
+
+  def create_charge
+    @charge = Stripe::Charge.create(
+        customer: @customer.id,
+        amount: default_amount,
+        description: "BigMoney Membership - #{current_user.email}",
+        currency: 'usd'
+    )
+  end
+
+  def save_charge
+    if @charge.present? && current_user.update_attributes(role: 'premium')
+      flash[:notice] = "Thanks for the payment #{current_user.email}! You can now create and edit private wikis!."
+      redirect_to root_path
+    else
+      flash[:alert] = "There appears to have been an error. Please try again."
+    end
+  end
+
+  def down_grade_to_standard
+    if current_user.update(role: "standard")
+      current_user.set_wikis_public
+      flash[:notice] = " You have downgraded your account to standard, #{current_user.email}!"
+      redirect_to root_path
+    else
+      flash[:alert] = "There appears to have been an error. Please try again."
+    end
+  end
+
+
+
+
 end
